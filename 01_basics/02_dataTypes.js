6570d312e6b4b4aaba4fe32769dc879c911c99d0
Subhajyoti Bhowmik <72068395+subhajyotiBhowmik@users.noreply.github.com>
2024-05-29 18:19:19 +0000

    datatypes

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,1 +1,31 @@
 "use strict"; //treat all JS code as newer version
+
+//alert("hello") //we are using nodejs ,not browser
+
+console.log(3+
+    3)//code readabililty issue 
+
+console.log("subhajyoti");
+
+let name  = "Subhajyoti"
+let age = 22
+let isLoggedin= false;
+let state;
+
+//PRIMITIVE DATATYPES ||
+
+// number => 2 to power 53
+// bigint 
+// string => " "
+// boolean => true /false 
+// null =>standalone value / representation of empty value / its also an object 
+// undefined 
+// symbol 
+
+// object 
+
+
+console.log(typeof"subhajyoti");
+console.log(typeof age );
+console.log(typeof null);
+console.log(typeof undefined);
