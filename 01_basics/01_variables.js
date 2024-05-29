44efbe9a2e5661fc0c3ed90b435edbceb7b07551
Subhajyoti Bhowmik <72068395+subhajyotiBhowmik@users.noreply.github.com>
2024-05-29 17:28:46 +0000

    Variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 13245
+let accountEmail = "subhajyoti@gmail.com"
+var accountPassword ='1122'
+accountCity = "Kolkata"
+let accountState;
+
+//accountId =2 //not allowed because accountId constant not changable 
+
+/*prefer not to use VAR because of issue in block scope and funcional scope*/
+
+accountEmail = "jubi@gmail.com"
+accountPassword = "21321"
+accountCity = "Kolkata"
+
+console.log(accountId);
+console.table([accountEmail,accountId,accountPassword,accountCity.accountState])
